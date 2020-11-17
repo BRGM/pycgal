2a4bd2d712ae1bd014ff912fe0c0b0d5f04a9d82
Simon Lopez <s.lopez@brgm.fr>
2020-11-17 11:51:12 +0100

    Revert to old setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-import sys
-import shutil
+import re
+from pathlib import Path
 
 try:
     from skbuild import setup
@@ -8,10 +8,29 @@ except ImportError:
     print("Install it running: python -m pip install scikit-build")
     sys.exit(1)
 
-# FIXME: For some strange reason when packages is specified in setup.cfg
-#        compiled modules are not included in the generated wheel.
-#        Is the content of the package directory parsed
-#        after compilation when passed as an argument here?
+
+def _find_version(name):
+    package_directory = Path(name)
+    assert package_directory.is_dir(), f"Could not find package directory: {name}"
+    init_file = package_directory / "__init__.py"
+    assert init_file.is_file(), f"Could not find package __init__.py"
+    for name, value in re.findall(
+        r"""__([a-z]+)__ = "([^"]+)""", init_file.read_text(encoding="utf8")
+    ):
+        if name == "version":
+            return value
+
+
+name = "pycgal"
+version = _find_version(name)
+
 setup(
-    packages=["pycgal",]
+    name=name,
+    version=version,
+    description="minimal CGAL wrap with pybind11",
+    author="brgm",
+    license="GPL v.3",
+    url="https://gitlab.brgm.fr/brgm/modelisation-geologique/pycgal",
+    packages=[name],
+    install_requires=["numpy >= 1.17"],
 )
