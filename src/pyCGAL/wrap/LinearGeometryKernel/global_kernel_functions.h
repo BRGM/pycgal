e3791622389bc09bbbec651cf46b7de6c1839f0e
Simon Lopez <s.lopez@brgm.fr>
2021-05-02 22:48:16 +0200

    Add Epick squared_distance
    To be completed.

--- src/pyCGAL/wrap/LinearGeometryKernel/global_kernel_functions.h
+++ src/pyCGAL/wrap/LinearGeometryKernel/global_kernel_functions.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <CGAL/Kernel/global_functions.h>
+#include <CGAL/squared_distance_2.h>
+#include <CGAL/squared_distance_3.h>
 #include <pyCGAL/typedefs.h>
 
 #include "detail/global_kernel_functions.h"
@@ -11,6 +13,7 @@ namespace detail = pyCGAL::wrap::LinearGeometryKernel::detail;
 
 template <typename Kernel>
 void wrap_element(detail::global_kernel_functions<Kernel>, py::module& module) {
+  using Point_2 = CGAL::Point_2<Kernel>;
   using Point_3 = CGAL::Point_3<Kernel>;
   using Vector_3 = CGAL::Vector_3<Kernel>;
 
@@ -23,6 +26,12 @@ void wrap_element(detail::global_kernel_functions<Kernel>, py::module& module) {
   module.def("cross_product",
              py::overload_cast<const Vector_3&, const Vector_3&>(
                  &CGAL::cross_product<Kernel>));
+  module.def("squared_distance",
+             py::overload_cast<const Point_2&, const Point_2&>(
+                 &CGAL::squared_distance<Kernel>));
+  module.def("squared_distance",
+             py::overload_cast<const Point_3&, const Point_3&>(
+                 &CGAL::squared_distance<Kernel>));
 }
 
 }  // namespace pyCGAL
