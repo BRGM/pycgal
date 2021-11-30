ee469030ae5ada5f2b67dd1b44418b3233b65678
Simon Lopez <s.lopez@brgm.fr>
2021-11-30 14:49:51 +0100

    Replace deprecated functions

--- src/pyCGAL/wrap/Surface_mesh/utils/io.h
+++ src/pyCGAL/wrap/Surface_mesh/utils/io.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <CGAL/Surface_mesh.h>
+#include <CGAL/Surface_mesh/IO/OFF.h>
 
 #include <fstream>
 
@@ -9,13 +9,13 @@ namespace pyCGAL::wrap::utils {
 template <typename P>
 bool read_off(CGAL::Surface_mesh<P>& sm, const std::string& filename) {
   std::ifstream is{filename};
-  return read_off(is, sm);
+  return CGAL::IO::read_OFF(is, sm);
 }
 
 template <typename P>
 bool write_off(const CGAL::Surface_mesh<P>& sm, const std::string& filename) {
   std::ofstream os{filename};
-  return write_off(os, sm);
+  return CGAL::IO::write_OFF(os, sm);
 }
 
 }  // namespace pyCGAL::wrap::utils
