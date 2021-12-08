269e6bf391de4f1766e7d522a5fa0b192a09f6df
Thomas Janvier, Simon Lopez <t.janvier@brgm.fr, s.lopez@brgm.fr>
2021-12-08 15:31:49 +0100

    Add PMP::clip parameters as keyword arguments

--- src/pyCGAL/wrap/Polygon_mesh_processing/clip.h
+++ src/pyCGAL/wrap/Polygon_mesh_processing/clip.h
@@ -15,18 +15,31 @@ template <typename TriangleMesh, typename Plane_3>
 void wrap_element(detail::clip<TriangleMesh, Plane_3>, py::module& module) {
   module.def(
       "clip",
-      [](TriangleMesh& tm, TriangleMesh& clipper) {
+      [](TriangleMesh& tm, TriangleMesh& clipper,
+         const bool throw_on_self_intersection, const bool clip_volume,
+         const bool use_compact_clipper, const bool do_not_modify) {
         if (!CGAL::is_triangle_mesh(tm) || !CGAL::is_triangle_mesh(clipper))
           throw std::runtime_error("Only triangle meshes can be clipped!");
-        CGAL::Polygon_mesh_processing::clip(tm, clipper);
+
+        namespace pns = CGAL::Polygon_mesh_processing::parameters;
+
+        return CGAL::Polygon_mesh_processing::clip(
+            tm, clipper,
+            pns::throw_on_self_intersection(throw_on_self_intersection)
+                .clip_volume(clip_volume)
+                .use_compact_clipper(use_compact_clipper)
+                .do_not_modify(do_not_modify));
       },
-      py::arg("tm").none(false), py::arg("clipper").none(false));
+      py::arg("tm").none(false), py::arg("clipper").none(false), py::kw_only(),
+      py::arg("throw_on_self_intersection") = false,
+      py::arg("clip_volume") = false, py::arg("use_compact_clipper") = false,
+      py::arg("do_not_modify") = false);
 
   module.def(
       "clip",
       [](TriangleMesh& mesh, const Plane_3& plane,
          const bool throw_on_self_intersection, const bool clip_volume,
-         const bool use_compact_clipper) {
+         const bool use_compact_clipper, const bool allow_self_intersections) {
         if (!CGAL::is_triangle_mesh(mesh))
           throw std::runtime_error("Only triangle meshes can be clipped!");
 
@@ -36,10 +49,12 @@ void wrap_element(detail::clip<TriangleMesh, Plane_3>, py::module& module) {
             mesh, plane,
             pns::throw_on_self_intersection(throw_on_self_intersection)
                 .clip_volume(clip_volume)
-                .use_compact_clipper(use_compact_clipper));
+                .use_compact_clipper(use_compact_clipper)
+                .allow_self_intersections(allow_self_intersections));
       },
-      py::arg("mesh").none(false), py::arg("plane").none(false),
+      py::arg("mesh").none(false), py::arg("plane").none(false), py::kw_only(),
       py::arg("throw_on_self_intersection") = false,
-      py::arg("clip_volume") = true, py::arg("use_compact_clipper") = false);
+      py::arg("clip_volume") = false, py::arg("use_compact_clipper") = true,
+      py::arg("allow_self_intersections") = false);
 }
 }  // namespace pyCGAL
