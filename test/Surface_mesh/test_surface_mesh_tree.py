9e53651e9805540f5a438c684d8ffae36087c05d
Simon Lopez <s.lopez@brgm.fr>
2022-07-28 16:33:49 +0200

    Add a segment mesh intersection example

--- test/Surface_mesh/test_surface_mesh_tree.py
+++ test/Surface_mesh/test_surface_mesh_tree.py
@@ -1,5 +1,5 @@
-from pycgal.Epick import Line_3, Point_3
-from pycgal.Surface_mesh import Surface_mesh
+from pycgal.Epick import Line_3, Point_3, Segment_3
+from pycgal.Surface_mesh import Surface_mesh, add_center_vertex
 from pycgal.Surface_mesh import Tree
 
 
@@ -18,3 +18,18 @@ def test_surface_mesh_tree(simple_mesh):
     line = Line_3(Point_3(-1, 0, 0), Point_3(1, 0, 0))
     intersections = tree.all_intersections(line)
     assert intersections and len(intersections) == 2
+
+
+def test_triangulate_segment_intersection(simple_mesh):
+
+    mesh = Surface_mesh(simple_mesh.all_vertices, simple_mesh.triangles)
+    tree = Tree(mesh)
+    S = Segment_3(Point_3(-1.1, 0, -1), Point_3(-1.1, 0, 1))
+    intersection = tree.any_intersection(S)
+    assert intersection
+    P, f = intersection
+    h = mesh.halfedge(f)
+    assert not mesh.is_border(h)
+    # WARNING: here we should test that P is not intersecting a face edge
+    h = add_center_vertex(h, mesh)
+    mesh[mesh.target(h)] = P
