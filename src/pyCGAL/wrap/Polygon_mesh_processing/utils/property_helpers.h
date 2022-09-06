7ac582f26ad5354231b098dfa7554ed2d531ac48
Simon Lopez <s.lopez@brgm.fr>
2022-09-06 17:06:27 +0200

    Optionnaly force the creation of property map

--- src/pyCGAL/wrap/Polygon_mesh_processing/utils/property_helpers.h
+++ src/pyCGAL/wrap/Polygon_mesh_processing/utils/property_helpers.h
@@ -11,7 +11,8 @@ namespace pyCGAL::wrap::utils {
 namespace py = pybind11;
 
 template <typename Index, typename T, typename Mesh>
-auto convert_to_property_map(py::object pmap, Mesh& mesh, T default_value) {
+auto convert_to_property_map(py::object pmap, Mesh& mesh, T default_value,
+                             bool throw_on_creation = false) {
   // It's ok to use std::optional as return types as Property_maps are actually
   // pointers
   using Property_map = typename Mesh::template Property_map<Index, T>;
@@ -19,15 +20,19 @@ auto convert_to_property_map(py::object pmap, Mesh& mesh, T default_value) {
   if (pmap.is_none()) return Result{};
   if (py::isinstance<py::str>(pmap)) {
     py::str name = pmap;
-    auto result = mesh.template add_property_map<Index>(name, default_value);
+    auto [map, created] =
+        mesh.template add_property_map<Index>(name, default_value);
 #ifndef NDEBUG
-    if (result.second) {
+    if (created) {
       py::print("Creating property map:", name);
     } else {
       py::print("Retrieving property map:", name);
     }
 #endif
-    return Result{result.first};
+    if (throw_on_creation && created) {
+      throw std::runtime_error("Property map creation was not allowed.");
+    }
+    return Result{map};
   }
   if (py::hasattr(pmap, "property_capsule")) {
     using Property_capsule =
