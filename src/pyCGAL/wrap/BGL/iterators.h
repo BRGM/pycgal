eed653c41125308b5d21c46c1478a22480a55806
Simon Lopez <s.lopez@brgm.fr>
2022-09-12 22:33:42 +0200

    More BGL iterators

--- src/pyCGAL/wrap/BGL/iterators.h
+++ src/pyCGAL/wrap/BGL/iterators.h
@@ -10,6 +10,35 @@ namespace pyCGAL {
 
 template <typename Graph>
 void wrap_element(wrap::BGL::detail::iterators<Graph>, py::module& module) {
+  using halfedge_descriptor =
+      typename boost::graph_traits<Graph>::halfedge_descriptor;
+  using vertex_descriptor =
+      typename boost::graph_traits<Graph>::vertex_descriptor;
+
+  wrap_class(
+      WrapTraits<
+          CGAL::Iterator_range<CGAL::Halfedge_around_source_iterator<Graph>>>{
+          "Halfedge_around_source_iterator"},
+      module);
+  module.def("halfedges_around_source",
+             py::overload_cast<halfedge_descriptor, const Graph&>(
+                 &CGAL::halfedges_around_source<Graph>));
+  module.def("halfedges_around_source",
+             py::overload_cast<vertex_descriptor, const Graph&>(
+                 &CGAL::halfedges_around_source<Graph>));
+
+  wrap_class(
+      WrapTraits<
+          CGAL::Iterator_range<CGAL::Halfedge_around_target_iterator<Graph>>>{
+          "Halfedge_around_target_iterator"},
+      module);
+  module.def("halfedges_around_target",
+             py::overload_cast<halfedge_descriptor, const Graph&>(
+                 &CGAL::halfedges_around_target<Graph>));
+  module.def("halfedges_around_target",
+             py::overload_cast<vertex_descriptor, const Graph&>(
+                 &CGAL::halfedges_around_target<Graph>));
+
   wrap_class(
       WrapTraits<
           CGAL::Iterator_range<CGAL::Halfedge_around_face_iterator<Graph>>>{
@@ -19,21 +48,35 @@ void wrap_element(wrap::BGL::detail::iterators<Graph>, py::module& module) {
 
   wrap_class(
       WrapTraits<
-          CGAL::Iterator_range<CGAL::Vertex_around_face_iterator<Graph>>>{
-          "Vertex_around_face_iterator"},
-      module);
-  module.def("vertices_around_face", &CGAL::vertices_around_face<Graph>);
-  module.def("vertices_around_face",
-             [](const typename Graph::Face_index& face, const Graph& graph) {
-               return CGAL::vertices_around_face(graph.halfedge(face), graph);
-             });
-
-  wrap_class(
-      WrapTraits<
           CGAL::Iterator_range<CGAL::Face_around_target_iterator<Graph>>>{
           "Face_around_target_iterator"},
       module);
   module.def("faces_around_target", &CGAL::faces_around_target<Graph>);
+
+  wrap_class(
+      WrapTraits<CGAL::Iterator_range<CGAL::Face_around_face_iterator<Graph>>>{
+          "Face_around_face_iterator"},
+      module);
+  module.def("faces_around_face", &CGAL::faces_around_face<Graph>);
+
+  wrap_class(
+      WrapTraits<
+          CGAL::Iterator_range<CGAL::Vertex_around_target_iterator<Graph>>>{
+          "Vertex_around_target_iterator"},
+      module);
+  module.def("vertices_around_target",
+             py::overload_cast<vertex_descriptor, const Graph&>(
+                 &CGAL::vertices_around_target<Graph>));
+  module.def("vertices_around_target",
+             py::overload_cast<vertex_descriptor, const Graph&>(
+                 &CGAL::vertices_around_target<Graph>));
+
+  wrap_class(
+      WrapTraits<
+          CGAL::Iterator_range<CGAL::Vertex_around_face_iterator<Graph>>>{
+          "Vertex_around_face_iterator"},
+      module);
+  module.def("vertices_around_face", &CGAL::vertices_around_face<Graph>);
 }
 
 }  // namespace pyCGAL
--- test/Surface_mesh/test_iterators_and_circulators.py
+++ test/Surface_mesh/test_iterators_and_circulators.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pytest
 
-from pycgal.Surface_mesh import Surface_mesh, halfedges_around_face
+from pycgal.Surface_mesh import (
+    Surface_mesh,
+    halfedges_around_face,
+    halfedges_around_source,
+    halfedges_around_target,
+)
 
 
 def test_halfedges_around_face(simple_mesh):
@@ -17,3 +22,10 @@ def test_halfedges_around_face(simple_mesh):
             assert h in l1
             l2.append(h)
         assert len(l1) == len(l2)
+
+    for v in mesh.vertices():
+        n1 = len(halfedges_around_source(v, mesh))
+        print(f"{v} is the source of {n1} halfedges")
+        h = mesh.halfedge(v)
+        n2 = len(halfedges_around_target(h, mesh))
+        assert n1 == n2
