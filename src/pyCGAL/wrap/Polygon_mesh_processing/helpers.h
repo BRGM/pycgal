4691af24d5715f12daae5231e8466a1cf37f67f9
Simon Lopez <s.lopez@brgm.fr>
2023-06-01 21:25:42 +0200

    Add PMP::is_outward_oriented

--- src/pyCGAL/wrap/Polygon_mesh_processing/helpers.h
+++ src/pyCGAL/wrap/Polygon_mesh_processing/helpers.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <CGAL/Polygon_mesh_processing/bbox.h>
+#include <CGAL/Polygon_mesh_processing/orientation.h>
 #include <CGAL/boost/graph/helpers.h>
 #include <pyCGAL/typedefs.h>
 
@@ -14,6 +15,8 @@ template <typename PolygonMesh>
 void wrap_element(detail::helpers<PolygonMesh>, py::module& module) {
   module.def("is_closed", &CGAL::is_closed<PolygonMesh>);
   module.def("is_triangle_mesh", &CGAL::is_triangle_mesh<PolygonMesh>);
+  module.def("is_outward_oriented",
+             &CGAL::Polygon_mesh_processing::is_outward_oriented<PolygonMesh>);
   module.def("is_quad_mesh", &CGAL::is_quad_mesh<PolygonMesh>);
   module.def("bbox", &CGAL::Polygon_mesh_processing::bbox<PolygonMesh>);
 }
