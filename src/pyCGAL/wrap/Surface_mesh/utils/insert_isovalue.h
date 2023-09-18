ef0e1a0ebf46e28f0d9af3a2abd798f5fadf4dfa
Simon Lopez <s.lopez@brgm.fr>
2023-09-18 22:26:52 +0200

    Refactor iso-value insertion

--- src/pyCGAL/wrap/Surface_mesh/utils/insert_isovalue.h
+++ src/pyCGAL/wrap/Surface_mesh/utils/insert_isovalue.h
@@ -16,10 +16,38 @@ using Vertex_flag = typename Surface_mesh::template Property_map<
     typename Surface_mesh::Vertex_index, bool>;
 
 template <typename Surface_mesh>
+using Edge_flag = typename Surface_mesh::template Property_map<
+    typename Surface_mesh::Edge_index, bool>;
+
+template <typename Surface_mesh>
+using Vertex_map = typename Surface_mesh::template Property_map<
+    typename Surface_mesh::Vertex_index, double>;
+
+template <typename Surface_mesh>
 struct Void_property_copier {
   using Edge_index = typename Surface_mesh::Edge_index;
   void operator()(Edge_index, Edge_index) {}
 };
+
+template <typename Surface_mesh, typename Insertions>
+void _build_insertion_points(const Surface_mesh& sm,
+                             const Vertex_map<Surface_mesh> fv,
+                             Vertex_flag<Surface_mesh> f_zero,
+                             Insertions& insertions) {
+  for (auto&& e : sm.edges()) {
+    auto v0 = sm.vertex(e, 0);
+    auto v1 = sm.vertex(e, 1);
+    double fv0 = fv[v0];
+    double fv1 = fv[v1];
+    if (fv0 == 0) f_zero[v0] = true;
+    if (fv1 == 0) f_zero[v1] = true;
+    if (fv0 * fv1 < 0) {
+      insertions.emplace_back(
+          e, CGAL::barycenter(sm.point(v0), fabs(fv1 / (fv1 - fv0)),
+                              sm.point(v1)));
+    }
+  }
+}
 
 template <typename Surface_mesh, typename Insertions,
           typename Property_copier = Void_property_copier<Surface_mesh>>
@@ -32,6 +60,18 @@ void _insert_points(Surface_mesh& sm, const Insertions& insertions,
     auto v = sm.target(h);
     sm.point(v) = I;
     f_zero[v] = true;
+  }
+}
+
+template <typename Surface_mesh>
+void _collect_constrained_edges(Surface_mesh& sm,
+                                const Vertex_flag<Surface_mesh> f_zero,
+                                Edge_flag<Surface_mesh> is_constrained) {
+  CGAL::Polygon_mesh_processing::triangulate_faces(sm);
+  for (auto&& e : sm.edges()) {
+    if (f_zero[sm.vertex(e, 0)] && f_zero[sm.vertex(e, 1)]) {
+      is_constrained[e] = true;
+    }
   }
 }
 
@@ -52,6 +92,7 @@ void insert_isovalue(Surface_mesh& sm, F&& f,
   auto&& [fv, fv_created] =
       sm.template add_property_map<Vertex_index, double>();
   assert(fv_created);
+
   for (auto&& v : sm.vertices()) {
     fv[v] = f(sm.point(v));
   }
@@ -59,20 +100,10 @@ void insert_isovalue(Surface_mesh& sm, F&& f,
   auto&& [f_zero, f_zero_created] =
       sm.template add_property_map<Vertex_index, bool>();
   assert(f_zero_created);
+
   std::vector<Insertion_point> insertions;
-  for (auto&& e : sm.edges()) {
-    Vertex_index v0 = sm.vertex(e, 0);
-    Vertex_index v1 = sm.vertex(e, 1);
-    double fv0 = fv[v0];
-    double fv1 = fv[v1];
-    if (fv0 == 0) f_zero[v0] = true;
-    if (fv1 == 0) f_zero[v1] = true;
-    if (fv0 * fv1 < 0) {
-      insertions.emplace_back(
-          e, CGAL::barycenter(sm.point(v0), fabs(fv1 / (fv1 - fv0)),
-                              sm.point(v1)));
-    }
-  }
+
+  _build_insertion_points(sm, fv, f_zero, insertions);
 
   if (optional_constrained_edges) {
     auto& is_constrained = *optional_constrained_edges;
@@ -81,14 +112,7 @@ void insert_isovalue(Surface_mesh& sm, F&& f,
         [&sm, is_constrained](const Edge_index& e1, const Edge_index& e2) {
           is_constrained[e2] = is_constrained[e1];
         });
-    // collect constrained edges
-    CGAL::Polygon_mesh_processing::triangulate_faces(sm);
-    auto& constrained_edges = *optional_constrained_edges;
-    for (auto&& e : sm.edges()) {
-      if (f_zero[sm.vertex(e, 0)] && f_zero[sm.vertex(e, 1)]) {
-        constrained_edges[e] = true;
-      }
-    }
+    _collect_constrained_edges(sm, f_zero, is_constrained);
   } else {
     _insert_points(sm, insertions, f_zero);
   }
